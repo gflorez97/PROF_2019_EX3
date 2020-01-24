8d2aeeed4e819d4f895c2e722160136a118c1eee
gflorez97 <gflorez97@gmail.com>
2020-01-24 16:15:38 +0100

    Se quita static del método getFutureTime para poder hacer mocking

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Time {
 	
-	public static String getFutureTime(long seconds) {
+	public String getFutureTime(long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime time = LocalDateTime.of(2020,1,1,0,0,0,0);
 		time = time.plusSeconds(seconds);
