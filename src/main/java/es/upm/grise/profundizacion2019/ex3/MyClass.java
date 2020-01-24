48454bd08581809b8e3e0a96bed0338d182efd84
gflorez97 <gflorez97@gmail.com>
2020-01-24 16:16:34 +0100

    time en MyClass pasa a requerir un objeto time, ahora que Time no es static

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -2,8 +2,8 @@ package es.upm.grise.profundizacion2019.ex3;
 
 public class MyClass {
 	
-	public void nextTime(long seconds) {
-		String next = Time.getFutureTime(seconds);
+	public void nextTime(Time time, long seconds) {
+		String next = time.getFutureTime(seconds);
 		System.out.println(next);
 	}
 
