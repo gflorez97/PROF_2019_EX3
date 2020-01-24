b761b90ce6066aff6619024835f30f9a724699e4
gflorez97 <gflorez97@gmail.com>
2020-01-24 15:43:15 +0100

    Cambiado el time a la fecha del 1 de enero en vez de la fecha actual

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -7,8 +7,8 @@ public class Time {
 	
 	public static String getFutureTime(long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
-		time.plusSeconds(seconds);
+		LocalDateTime time = LocalDateTime.of(2020,1,1,0,0,0,0);
+		time = time.plusSeconds(seconds);
 		return formatter.format(time);	
 	}
 
