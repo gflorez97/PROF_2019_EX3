175ef57766fdfda95144ad0d83920c256023f6c2
gflorez97 <gflorez97@gmail.com>
2020-01-24 15:32:40 +0100

    En Time, es necesario cambiar DateTime now por la LocalDateTime correspondiente al 1 de enero

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -7,7 +7,7 @@ public class Time {
 	
 	public static String getFutureTime(long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
+		LocalDateTime time = LocalDateTime.of(2020,1,1,0,2,0,0);
 		time.plusSeconds(seconds);
 		return formatter.format(time);	
 	}
